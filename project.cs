8bb067afbabf118bdfad58800fab1c3872ebf25a
gengzhiyao <2473792618@qq.com>
2024-03-07 15:27:23 +0800

    second commit

--- project.cs
+++ project.cs
@@ -198,3 +198,119 @@ Writing objects: 100 % (3 / 3), 298 bytes | 298.00 KiB / s, done.
 Total 3(delta 0), reused 0 (delta 0), pack-reused 0
 To github.com:gengzhiyao / learn.git
    0834aa1..10fb67a main -> main
+
+
+geng@LAPTOP MINGW64 /e/App_Data/Unity_Project/UnityLearn
+$ git init
+Initialized empty Git repository in E:/ App_Data / Unity_Project / UnityLearn /.git /
+
+geng@LAPTOP MINGW64 /e/App_Data/Unity_Project/UnityLearn (master)
+$ git status
+On branch master
+
+No commits yet
+
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+        Hello.c
+
+nothing added to commit but untracked files present (use "git add" to track)
+
+geng@LAPTOP MINGW64 /e/App_Data/Unity_Project/UnityLearn (master)
+$ git commit -m "first commit"
+On branch master
+
+Initial commit
+
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+        Hello.c
+
+nothing added to commit but untracked files present (use "git add" to track)
+
+geng@LAPTOP MINGW64 /e/App_Data/Unity_Project/UnityLearn (master)
+$ git remote add Test git@github.com:gengzhiyao / learn.git
+
+geng @LAPTOP MINGW64 /e/App_Data/Unity_Project/UnityLearn (master)
+$ git pull --rebase Test main
+fatal: Updating an unborn branch with changes added to the index.
+
+geng@LAPTOP MINGW64 /e/App_Data/Unity_Project/UnityLearn (master)
+$ git pull
+remote: Enumerating objects: 16, done.
+remote: Counting objects: 100 % (16 / 16), done.
+remote: Compressing objects: 100 % (12 / 12), done.
+remote: Total 16 (delta 2), reused 10 (delta 1), pack-reused 0
+Unpacking objects: 100 % (16 / 16), 18.35 KiB | 268.00 KiB / s, done.
+From github.com: gengzhiyao / learn
+ * [new branch]      main->Test / main
+There is no tracking information for the current branch.
+Please specify which branch you want to merge with.
+See git-pull(1) for details.
+
+    git pull <remote> <branch>
+
+If you wish to set tracking information for this branch you can do so with:
+
+    git branch --set-upstream-to=Test/<branch> master
+
+
+geng@LAPTOP MINGW64 /e/App_Data/Unity_Project/UnityLearn (master)
+$ git pull Test main
+From github.com:gengzhiyao / learn
+ * branch            main->FETCH_HEAD
+
+geng @LAPTOP MINGW64 /e/App_Data/Unity_Project/UnityLearn (master)
+$ git push Test master:main
+Everything up-to-date
+
+geng@LAPTOP MINGW64 /e/App_Data/Unity_Project/UnityLearn (master)
+$ git push Test master:main
+Everything up-to-date
+
+geng@LAPTOP MINGW64 /e/App_Data/Unity_Project/UnityLearn (master)
+$ git commit -m "first commit"
+On branch master
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+        Hello.c
+
+nothing added to commit but untracked files present (use "git add" to track)
+
+geng@LAPTOP MINGW64 /e/App_Data/Unity_Project/UnityLearn (master)
+$ git push Test master:main
+Everything up-to-date
+
+geng@LAPTOP MINGW64 /e/App_Data/Unity_Project/UnityLearn (master)
+$ git status
+On branch master
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+        Hello.c
+
+nothing added to commit but untracked files present (use "git add" to track)
+
+geng@LAPTOP MINGW64 /e/App_Data/Unity_Project/UnityLearn (master)
+$ git add .
+
+geng@LAPTOP MINGW64 /e/App_Data/Unity_Project/UnityLearn (master)
+$ git push Test master:main
+Everything up-to-date
+
+geng@LAPTOP MINGW64 /e/App_Data/Unity_Project/UnityLearn (master)
+$ git commit -m "first commit"
+[master 723a78e] first commit
+ 1 file changed, 6 insertions(+)
+ create mode 100644 Hello.c
+
+geng@LAPTOP MINGW64 /e/App_Data/Unity_Project/UnityLearn (master)
+$ git push Test master:main
+Enumerating objects: 4, done.
+Counting objects: 100 % (4 / 4), done.
+Delta compression using up to 20 threads
+Compressing objects: 100 % (3 / 3), done.
+Writing objects: 100 % (3 / 3), 314 bytes | 314.00 KiB / s, done.
+Total 3(delta 1), reused 0 (delta 0), pack-reused 0
+remote: Resolving deltas: 100 % (1 / 1), completed with 1 local object.
+To github.com:gengzhiyao / learn.git
+   607efd3..723a78e master -> main
